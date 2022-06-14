5831c434eafcfb1fe97bc47d4ff6792965ef628f
JKLLR <jeffhuria@gmail.com>
2022-06-14 03:07:58 +0300

    add search input in navbar

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('profile/',views.project,name = 'profile'),
     path('project/',views.project,name = 'project'),
     path('add_project/',views.add_project,name = 'addproject'),
-
+    path('rate_project/(<image_id>\d+)',views.rate_project,name = 'rate_project'),
+    path('search_project/(<image_id>\d+)',views.search_project,name = 'search_project'),
 ]
