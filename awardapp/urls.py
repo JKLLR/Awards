6a8923e9699e95e5e9c73e81e49f3cf719931679
JKLLR <jeffhuria@gmail.com>
2022-06-14 03:10:05 +0300

    add search view function

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
     path('project/',views.project,name = 'project'),
     path('add_project/',views.add_project,name = 'addproject'),
     path('rate_project/(<image_id>\d+)',views.rate_project,name = 'rate_project'),
-    path('search_project/(<image_id>\d+)',views.search_project,name = 'search_project'),
+    path('search_project/(<image_id>\d+)',views.project_search,name = 'search_project'),
 ]
